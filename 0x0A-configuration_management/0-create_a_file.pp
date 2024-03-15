fa4b8c31bd3ee5d22cc231dda611de679c547ba5
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-03-15 12:09:57 +0100

    Correct errors

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 #Puppet creates a file, yay
 file { '/tmp/school':
   path    => '/tmp/school',
-  mode    => "0744",
+  mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
   content => 'I love Puppet',
--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,4 @@
 # Installs Flask version 2.1.0
 exec { 'Install flask':
   command     => '/usr/bin/pip3 install flask==2.1.0',
-  refreshonly => true
 }
