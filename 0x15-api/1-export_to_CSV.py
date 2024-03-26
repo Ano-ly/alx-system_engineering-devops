347618ed9393be3fe89d17962335bc0b70426ea6
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-03-26 11:30:30 +0100

    Tasks 0, 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,33 +1,40 @@
 #!/usr/bin/python3
-"""Fetch data via a RestAPI"""
-import requests
-import sys
+"""
+Fetch data via a RestAPI, jsonplaceholder and write
+to csv file
+"""
 
-my_url_todo = 'https://jsonplaceholder.typicode.com/todos'
-my_url_user = 'https://jsonplaceholder.typicode.com/users'
+if __name__ == "__main__":
+    """Don't run script if imported"""
+    import requests
+    import sys
+    import csv
 
-todo_data = requests.get(my_url_todo).json()
-user_data = requests.get(my_url_user).json()
+    my_url_todo = 'https://jsonplaceholder.typicode.com/todos'
+    my_url_user = 'https://jsonplaceholder.typicode.com/users'
 
-done_titles = []
-tasks = []
-name = ""
+    todo_data = requests.get(my_url_todo).json()
+    user_data = requests.get(my_url_user).json()
 
-for dic in user_data:
-    if str(dic.get("id")) == sys.argv[1]:
-        name = dic.get("name")
-for dic in todo_data:
-    if str(dic.get("user.Id")) == sys.argv[1]:
-        tasks.append(dic)
-for task in tasks:
-    if task.get("completed"):
-        done_titles.append(task.get("title"))
+    tasks = []
+    name = ""
 
-top_str = "Employee {} is done with tasks ({}/{}):"\
-          .format(name, len(done_titles), len(tasks))
-content = "\n\t ".join(done_titles)
-
-print(top_str)
-print("\t ", end="")
-
-print(content)
+    for dic in user_data:
+        if str(dic.get("id")) == sys.argv[1]:
+            name = dic.get("name")
+            username = dic.get("username")
+    for dic in todo_data:
+        if str(dic.get("userId")) == sys.argv[1]:
+            tasks.append(dic)
+    str_lists = []
+    with open ("USER_ID.csv", "w") as csvfile:
+        csv_obj = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for task in tasks:
+            if task.get("completed"):
+                status = "True"
+            else:
+                status = "False"
+            title = task.get("title")
+            str_list = [sys.argv[1], username, status, title]
+            str_lists.append(str_list)
+        csv_obj.writerows(str_lists)
