926d51e4167f6e2d697b80aa494f441b1e928ee1
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-03-26 06:02:03 +0100

    API project

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,33 +1,35 @@
 #!/usr/bin/python3
 """Fetch data via a RestAPI"""
-import requests
-import sys
 
-my_url_todo = 'https://jsonplaceholder.typicode.com/todos'
-my_url_user = 'https://jsonplaceholder.typicode.com/users'
+if __name__ == "__main__":
+    import requests
+    import sys
 
-todo_data = requests.get(my_url_todo).json()
-user_data = requests.get(my_url_user).json()
+    my_url_todo = 'https://jsonplaceholder.typicode.com/todos'
+    my_url_user = 'https://jsonplaceholder.typicode.com/users'
 
-done_titles = []
-tasks = []
-name = ""
+    todo_data = requests.get(my_url_todo).json()
+    user_data = requests.get(my_url_user).json()
 
-for dic in user_data:
-    if str(dic.get("id")) == sys.argv[1]:
-        name = dic.get("name")
-for dic in todo_data:
-    if str(dic.get("userId")) == sys.argv[1]:
-        tasks.append(dic)
-for task in tasks:
-    if task.get("completed"):
-        done_titles.append(task.get("title"))
+    done_titles = []
+    tasks = []
+    name = ""
 
-top_str = "Employee {} is done with tasks({}/{}):"\
-          .format(name, len(done_titles), len(tasks))
-content = "\n\t ".join(done_titles)
+    for dic in user_data:
+        if str(dic.get("id")) == sys.argv[1]:
+            name = dic.get("name")
+    for dic in todo_data:
+        if str(dic.get("userId")) == sys.argv[1]:
+            tasks.append(dic)
+    for task in tasks:
+        if task.get("completed"):
+            done_titles.append(task.get("title"))
 
-print(top_str)
-print("\t ", end="")
+    top_str = "Employee {} is done with tasks({}/{}):"\
+              .format(name, len(done_titles), len(tasks))
+    content = "\n\t ".join(done_titles)
 
-print(content)
+    print(top_str)
+    print("\t ", end="")
+
+    print(content)
