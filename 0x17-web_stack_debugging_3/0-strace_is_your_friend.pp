2d39ac17ddaa26d1c9a538b377bbaf80710df191
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-04-13 03:20:43 +0100

    Right content

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,12 @@
 # Fix Apache on an Ubuntu Machine
 file { '/var/www/html/index.html':
   ensure  => file,
-  content => '<!DOCTYPE HTML><HTML></HTML>',
+  content => '<!DOCTYPE HTML><html><title>Holberton &#8211; Just another WordPress site</title>
+<link rel="alternate" type="application/rss+xml" title="Holberton &raquo; Feed" href="http://127.0.0.1/?feed=rss2" />
+<link rel="alternate" type="application/rss+xml" title="Holberton &raquo; Comments Feed" href="http://127.0.0.1/?feed=comments-rss2" />
+        <div id="wp-custom-header" class="wp-custom-header"><img src="http://127.0.0.1/wp-content/themes/twentyseventeen/assets/images/header.jpg" width="2000" height="1200" alt="Holberton" /></div>  </div>
+                            <h1 class="site-title"><a href="http://127.0.0.1/" rel="home">Holberton</a></h1>
+        <p>Yet another bug by a Holberton student</p></html>',
   owner   => 'root',
   group   => 'root',
   mode    => '0644',
